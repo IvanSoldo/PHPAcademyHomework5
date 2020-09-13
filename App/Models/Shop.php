31b50572704f10a884351ceddca5c7438275cf50
IvanSoldo <ivandragosoldo@gmail.com>
2020-09-13 21:56:24 +0200

    Minor change

--- App/Models/Shop.php
+++ App/Models/Shop.php
@@ -3,15 +3,12 @@
 namespace App\Models;
 
 require_once 'App/Core/ViewableInterface.php';
-require_once 'App/Core/PrefixAdder.php';
+require_once 'App/Core/AbstractModel.php';
 
 use \App\Core\ViewableInterface;
-use \App\Core\PrefixAdder;
+use \App\Core\AbstractModel;
 
-class Shop extends PrefixAdder implements ViewableInterface{
+class Shop extends AbstractModel implements ViewableInterface{
 
-    public function viewAction()
-    {
-        return $this->addPrefix() . ' ' . get_class($this);
-    }
+
 }
